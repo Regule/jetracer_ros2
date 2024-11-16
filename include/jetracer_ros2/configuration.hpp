41302b8fa3d197195ef31281605382eb19e1db90
Regule <regule@runbox.com>
2024-11-16 16:13:46 +0100

    Initial commit

---
+++ include/jetracer_ros2/configuration.hpp
@@ -0,0 +1,19 @@
+#ifndef JETRACER_ROS2__CONFIGURATION_HPP_
+#define JETRACER_ROS2__CONFIGURATION_HPP_
+
+#include "jetracer_ros2/visibility_control.h"
+
+namespace jetracer_ros2
+{
+
+class Configuration
+{
+public:
+  Configuration();
+
+  virtual ~Configuration();
+};
+
+}  // namespace jetracer_ros2
+
+#endif  // JETRACER_ROS2__CONFIGURATION_HPP_
---
+++ src/configuration.cpp
@@ -0,0 +1,14 @@
+#include "jetracer_ros2/configuration.hpp"
+
+namespace jetracer_ros2
+{
+
+Configuration::Configuration()
+{
+}
+
+Configuration::~Configuration()
+{
+}
+
+}  // namespace jetracer_ros2
